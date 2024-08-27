c85f0309cac7df8525ffc68d1645e4312470462f
kawkaw5000 <lexeramorcillo@gmail.com>
2024-08-28 00:27:55 +0800

    Added Modals design

--- js/selectuser.js
+++ js/selectuser.js
@@ -1,17 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('.blank-profile-container').style.display = 'block';
+  document.querySelector('.selected-profile-container').style.display = 'none';
+  document.querySelector('.edit-profile-container').style.display = 'none';
+
   document.querySelectorAll(".user-list-table tbody tr").forEach(row => {
       row.addEventListener("click", function() {
-          const userId = this.getAttribute("data-id");
-          const accountType = this.getAttribute("data-accounttype");
-          const username = this.getAttribute("data-username");
-          const email = this.getAttribute("data-email");
-
-          document.getElementById("userId").value = userId;
-          document.getElementById("accountType").value = accountType;
-          document.getElementById("username").value = username;
-          document.getElementById("email").value = email;
+          showUserDetails(row);
       });
   });
 });
 
+function showUserDetails(row) {
 
+  document.querySelector('.blank-profile-container').style.display = 'none';
+  document.querySelector('.edit-profile-container').style.display = 'none';
+  document.querySelector('.selected-profile-container').style.display = 'block';
+ 
+  document.getElementById('userId').value = row.dataset.id;
+  document.getElementById('accountType').value = row.dataset.accounttype;
+  document.getElementById('username').value = row.dataset.username;
+  document.getElementById('email').value = row.dataset.email;
+}
+
+function showEditUserDetails(row) {
+  document.querySelector('.blank-profile-container').style.display = 'none';
+  document.querySelector('.selected-profile-container').style.display = 'none';
+  document.querySelector('.edit-profile-container').style.display = 'block';
+  document.getElementById('editUserId').value = row.dataset.id;
+  document.getElementById('editAccountType').value = row.dataset.accounttype;
+  document.getElementById('editUsername').value = row.dataset.username;
+  document.getElementById('editEmail').value = row.dataset.email;
+}
+
+document.querySelectorAll('.dropdown-item').forEach(item => {
+  item.addEventListener('click', function(event) {
+      if (this.textContent === 'Edit') {
+          event.stopPropagation();
+
+          const selectedRow = this.closest('tr');
+          showEditUserDetails(selectedRow);
+          
+          const dropdownContent = this.closest('.dropdown-content');
+          dropdownContent.classList.remove('show');
+      }
+  });
+});
