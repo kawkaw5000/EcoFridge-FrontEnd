fe317233d698fe6c71d1a5aca46dda9d5443f7f0
kawkaw5000 <lexeramorcillo@gmail.com>
2024-08-29 17:41:42 +0800

    Done with dashboard

--- js/login.js
+++ js/login.js
@@ -11,3 +11,16 @@ function togglePasswordVisibility() {
   }
 }
 
+
+function toggleConfirmPasswordVisibility() {
+  const passwordInput = document.getElementById("confirm-password");
+  const eyeIcon = document.getElementById("eyeconfirm-icon");
+  
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    eyeIcon.src = "/LoginImg/eye-password-hide-svgrepo-com.svg";
+  } else {
+    passwordInput.type = "password";
+    eyeIcon.src = "/LoginImg/eye-close-up-svgrepo-com.svg";
+  }
+}
